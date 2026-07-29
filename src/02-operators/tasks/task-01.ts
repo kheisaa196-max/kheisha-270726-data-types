fed4b564fa63a8896de9ad6eefea63f214592ec1
kheishaa <kheisaa196@gmail.com>
2026-07-29 11:57:42 +0700

    complete data types tasks

--- src/02-operators/tasks/task-01.ts
+++ src/02-operators/tasks/task-01.ts
@@ -14,3 +14,21 @@
  *  - Final payment
  * 2. Display the calculation results.
  */
+let friedrice = 3;
+let pricefreidrice = 18000;
+
+let drink = 2
+let pricedrink= 5000;
+
+let discount= 10000;
+
+let totalfriedrice = friedrice *pricefreidrice;
+let totaldrink = drink *  pricedrink;
+let grantotal=totalfriedrice + totaldrink;
+let finalpayment=grantotal - discount;
+
+console.log("=== calculation===")
+console.log ("Total fried rice : Rp"+totalfriedrice);
+console.log("Total drink : Rp"+totaldrink);
+console.log("Grandtotal :Rp"+grantotal);
+console.log("finalpayment :Rp"+finalpayment);
